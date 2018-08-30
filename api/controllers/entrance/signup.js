98e37a082ff05c775645e1234148faef02a6b4c6
devstorm1027 <cb.wglobe@gmail.com>
2018-08-30 14:05:10 +0200

    built the signup controller

--- api/controllers/entrance/signup.js
+++ api/controllers/entrance/signup.js
@@ -19,13 +19,13 @@ the account verification message.)`,
 
   inputs: {
 
-    // emailAddress: {
-    //   required: true,
-    //   type: 'string',
-    //   isEmail: true,
-    //   description: 'The email address for the new account, e.g. m@example.com.',
-    //   extendedDescription: 'Must be a valid email address.',
-    // },
+    emailAddress: {
+      required: true,
+      type: 'string',
+      isEmail: true,
+      description: 'The email address for the new account, e.g. m@example.com.',
+      extendedDescription: 'Must be a valid email address.',
+    },
 
     password: {
       required: true,
@@ -35,19 +35,12 @@ the account verification message.)`,
       description: 'The unencrypted password to use for the new account.'
     },
 
-    // fullName:  {
-    //   required: true,
-    //   type: 'string',
-    //   example: 'Frida Kahlo de Rivera',
-    //   description: 'The user\'s full name.',
-    // },
-
-    userName:  {
+    fullName:  {
       required: true,
       type: 'string',
       example: 'Frida Kahlo de Rivera',
       description: 'The user\'s full name.',
-    }
+    },
 
   },
 
@@ -61,47 +54,45 @@ the account verification message.)`,
       'parameters should have been validated/coerced _before_ they were sent.'
     },
 
-    // emailAlreadyInUse: {
-    //   statusCode: 409,
-    //   description: 'The provided email address is already in use.',
-    // },
+    emailAlreadyInUse: {
+      statusCode: 409,
+      description: 'The provided email address is already in use.',
+    },
 
   },
 
 
   fn: async function (inputs, exits) {
 
-    // var newEmailAddress = inputs.emailAddress.toLowerCase();
+    var newEmailAddress = inputs.emailAddress.toLowerCase();
 
     // Build up data for the new user record and save it to the database.
     // (Also use `fetch` to retrieve the new ID so that we can use it below.)
     var newUserRecord = await User.create(Object.assign({
-      // emailAddress: newEmailAddress,
-      userName: inputs.userName,
+      emailAddress: newEmailAddress,
       password: await sails.helpers.passwords.hashPassword(inputs.password),
-      // fullName: inputs.fullName,
+      fullName: inputs.fullName,
       tosAcceptedByIp: this.req.ip
     }, 
-    // sails.config.custom.verifyEmailAddresses? {
-    //   emailProofToken: await sails.helpers.strings.random('url-friendly'),
-    //   emailProofTokenExpiresAt: Date.now() + sails.config.custom.emailProofTokenTTL,
-    //   emailStatus: 'unconfirmed'
-    // }:{}
-    ))
-    // .intercept('E_UNIQUE', 'emailAlreadyInUse')
+    sails.config.custom.verifyEmailAddresses? {
+      emailProofToken: await sails.helpers.strings.random('url-friendly'),
+      emailProofTokenExpiresAt: Date.now() + sails.config.custom.emailProofTokenTTL,
+      emailStatus: 'unconfirmed'
+    }:{}))
+    .intercept('E_UNIQUE', 'emailAlreadyInUse')
     .intercept({name: 'UsageError'}, 'invalid')
     .fetch();
 
     // If billing feaures are enabled, save a new customer entry in the Stripe API.
     // Then persist the Stripe customer id in the database.
-    // if (sails.config.custom.enableBillingFeatures) {
-    //   let stripeCustomerId = await sails.helpers.stripe.saveBillingInfo.with({
-    //     emailAddress: newEmailAddress
-    //   });
-    //   await User.update(newUserRecord.id).set({
-    //     stripeCustomerId
-    //   });
-    // }
+    if (sails.config.custom.enableBillingFeatures) {
+      let stripeCustomerId = await sails.helpers.stripe.saveBillingInfo.with({
+        emailAddress: newEmailAddress
+      });
+      await User.update(newUserRecord.id).set({
+        stripeCustomerId
+      });
+    }
 
     // Store the user's new id in their session.
     this.req.session.userId = newUserRecord.id;
